51f5916fddcf5c47a5c70152a1ca54a61094784f
PhillipRothenbeck <philliprothenbeck@gmail.com>
2025-10-17 22:21:15 +0200

    add recognizing exhibits of interest

--- Assets/Scripts/ParentController.cs
+++ Assets/Scripts/ParentController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class ParentController : MonoBehaviour
 {
-    public ExhibitController target;
+    public GameObject target;
     public String[] interests;
     public float speed = 1.0f;
 
@@ -16,10 +16,22 @@ public class ParentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(target != null)
+        if (target != null)
         {
             Vector3 targetPosition = target.transform.position;
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+        
+            if (Vector3.Distance(transform.position, target.transform.position) < 0.1f)
+            {
+                if (Array.Exists(interests, interest => interest == target.GetComponent<ExhibitController>().exhibit.Interest))
+                {
+                    Debug.Log("Reached target exhibit of interest: " + target.GetComponent<ExhibitController>().exhibit.Interest);
+                }
+                else
+                {
+                    Debug.Log("Reached target exhibit, but not of interest: " + target.GetComponent<ExhibitController>().exhibit.Interest);
+                }
+            }
         }
 
 
@@ -32,7 +44,7 @@ public class ParentController : MonoBehaviour
 
             // select a random exhibit
             ExhibitController randomExhibit = exhibits[UnityEngine.Random.Range(0, exhibits.Length)];
-            target = randomExhibit;
+            target = randomExhibit.gameObject;
 
         }
     }
